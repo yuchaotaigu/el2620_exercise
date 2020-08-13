89577f7d8a295c4af9e657c504cecfd853a402af
yuchaotaigu <yuchaotaigu@gmail.com>
2020-08-13 21:19:45 +0200

    change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = u'el2620_exercise'
+project = u'EL2620 Exercise'
 copyright = u'2020, Yuchao Li'
 author = u'Yuchao Li'
 
@@ -75,7 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
